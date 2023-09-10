5ae93fd8e8eee38360e669519ae5b196704b9c9c
Gian Ortiz <ortizgianaugusto@gmail.com>
2023-09-10 09:03:56 -0300

    Refactor creation of Interceptor

---
+++ cmd/interceptor/interceptor/interceptor.go
@@ -0,0 +1,78 @@
+package interceptor
+
+import (
+	interceptorConfig "github.com/GianOrtiz/k8s-transparent-checkpoint-restore/internal/config/interceptor"
+	"github.com/GianOrtiz/k8s-transparent-checkpoint-restore/internal/delivery"
+	"github.com/GianOrtiz/k8s-transparent-checkpoint-restore/internal/entity"
+	"github.com/GianOrtiz/k8s-transparent-checkpoint-restore/internal/repository/interceptedrequest"
+	"github.com/GianOrtiz/k8s-transparent-checkpoint-restore/internal/service/checkpoint"
+	"github.com/GianOrtiz/k8s-transparent-checkpoint-restore/internal/service/scheduler"
+	"github.com/GianOrtiz/k8s-transparent-checkpoint-restore/internal/service/statemanager"
+	"github.com/GianOrtiz/k8s-transparent-checkpoint-restore/internal/usecase"
+	"github.com/google/uuid"
+)
+
+type Interceptor struct {
+	*entity.Interceptor
+	useCase   usecase.InterceptorUseCase
+	scheduler usecase.Scheduler
+}
+
+func New() (*Interceptor, error) {
+	interceptorId := uuid.NewString()
+	monitoredContainerID := uuid.NewString()
+
+	config, err := interceptorConfig.FromEnv()
+	if err != nil {
+		return nil, err
+	}
+
+	interceptor := entity.Interceptor{
+		ID:                    interceptorId,
+		MonitoringContainerID: monitoredContainerID,
+		MonitoredContainer: &entity.Container{
+			ID:      monitoredContainerID,
+			PID:     272389,
+			HTTPUrl: config.ContainerURL.String(),
+			Name:    config.ContainerName,
+		},
+		Config: config,
+	}
+
+	// TODO: make checkpoint service be selected via environment.
+	checkpointService, err := checkpoint.CRIU(checkpoint.CRIUCheckpointServiceConfig{
+		ImagesDirectory: "/home/gian/test-images",
+	})
+	if err != nil {
+		return nil, err
+	}
+	// TODO: make scheduler be selected via environment.
+	scheduler := scheduler.Local()
+	// TODO: make state manager service be selected via environment.
+	stateManagerService := statemanager.AlawaysAcceptingStub()
+	// TODO: make intercepted request repository be selected via environment
+	interceptedRequestRepository := interceptedrequest.InMemory()
+	interceptorUseCase, err := usecase.Interceptor(&interceptor, checkpointService, stateManagerService, interceptedRequestRepository, scheduler)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Interceptor{
+		useCase:     interceptorUseCase,
+		Interceptor: &interceptor,
+		scheduler:   scheduler,
+	}, nil
+}
+
+func (i *Interceptor) Run() error {
+	go func(interceptorUseCase usecase.InterceptorUseCase) {
+		i.scheduler.ScheduleCheckpoint(interceptorUseCase, i.Config.CheckpointingInterval)
+	}(i.useCase)
+
+	interceptorServer := delivery.InterceptorServer(8001, i.useCase)
+	if err := interceptorServer.Run(); err != nil {
+		return err
+	}
+
+	return nil
+}
--- cmd/interceptor/main.go
+++ cmd/interceptor/main.go
@@ -1,57 +1,14 @@
 package main
 
-import (
-	"net/url"
-	"time"
-
-	"github.com/GianOrtiz/k8s-transparent-checkpoint-restore/internal/config/interceptor"
-	"github.com/GianOrtiz/k8s-transparent-checkpoint-restore/internal/delivery"
-	"github.com/GianOrtiz/k8s-transparent-checkpoint-restore/internal/entity"
-	"github.com/GianOrtiz/k8s-transparent-checkpoint-restore/internal/repository/interceptedrequest"
-	"github.com/GianOrtiz/k8s-transparent-checkpoint-restore/internal/service/checkpoint"
-	"github.com/GianOrtiz/k8s-transparent-checkpoint-restore/internal/service/scheduler"
-	"github.com/GianOrtiz/k8s-transparent-checkpoint-restore/internal/service/statemanager"
-	"github.com/GianOrtiz/k8s-transparent-checkpoint-restore/internal/usecase"
-	"github.com/google/uuid"
-)
+import "github.com/GianOrtiz/k8s-transparent-checkpoint-restore/cmd/interceptor/interceptor"
 
 func main() {
-	monitoredContainerID := uuid.NewString()
-	interceptor := entity.Interceptor{
-		ID:                    uuid.NewString(),
-		MonitoringContainerID: monitoredContainerID,
-		MonitoredContainer: &entity.Container{
-			ID:      monitoredContainerID,
-			PID:     272389,
-			HTTPUrl: "http://localhost:8000",
-			Name:    "test",
-		},
-		Config: &interceptor.Config{
-			CheckpointingInterval: time.Duration(time.Minute * 20),
-			ContainerURL:          url.URL{},
-			ContainerPID:          272389,
-			ContainerName:         "test",
-			StateManagerURL:       url.URL{},
-		},
-	}
-	checkpointService, err := checkpoint.CRIU(checkpoint.CRIUCheckpointServiceConfig{
-		ImagesDirectory: "/home/gian/test-images",
-	})
-	if err != nil {
-		panic(err)
-	}
-	scheduler := scheduler.Local()
-	stateManagerService := statemanager.AlawaysAcceptingStub()
-	interceptedRequestRepository := interceptedrequest.InMemory()
-	interceptorUseCase, err := usecase.Interceptor(&interceptor, checkpointService, stateManagerService, interceptedRequestRepository, scheduler)
+	interceptor, err := interceptor.New()
 	if err != nil {
 		panic(err)
 	}
 
-	go func(interceptorUseCase usecase.InterceptorUseCase) {
-		scheduler.ScheduleCheckpoint(interceptorUseCase, interceptor.Config.CheckpointingInterval)
-	}(interceptorUseCase)
-
-	interceptorServer := delivery.InterceptorServer(8001, interceptorUseCase)
-	interceptorServer.Run()
+	if err := interceptor.Run(); err != nil {
+		panic(err)
+	}
 }
